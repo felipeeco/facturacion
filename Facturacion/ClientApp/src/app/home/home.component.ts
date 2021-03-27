bb6c8c8ee23edbab6dbc2206a558179f553e8379
felipeeco <contacto@felipemoreno.co>
2021-03-26 20:11:50 -0500

    falta el modal de mensaje exitoso

--- Facturacion/ClientApp/src/app/home/home.component.ts
+++ Facturacion/ClientApp/src/app/home/home.component.ts
@@ -9,22 +9,36 @@ import { ProductServiceService } from '../services/product-service.service';
 export class HomeComponent {
 
   formCreateProduct: FormGroup;
+  formCreateProductOBJ = {
+    name: false,
+    price: false,
+    stock: false
+  };
 
   constructor(private productService: ProductServiceService) {
     this.formCreateProduct = new FormGroup
       ({
-        'name': new FormControl("", [Validators.required, Validators.maxLength(50), Validators.pattern("^[a-zA-ZÀ-ÖØ-öø-ÿ]+$")]),
-        'price': new FormControl(0, [Validators.required]),
-        'stock': new FormControl(0, [Validators.required])
+        'name': new FormControl("", [Validators.required]),
+        'price': new FormControl(0, [Validators.required, Validators.min(1000)]),
+        'stock': new FormControl(0, [Validators.required, Validators.min(1)])
       });
   }
 
-  submitCreateProduct() {
-    if (this.formCreateProduct.valid == true) {
+  submitCreateProduct(){
+    if (this.formCreateProduct.valid == false){
+      if (this.formCreateProduct.controls["name"].touched == false && this.formCreateProduct.controls["name"].errors.required == true) {
+        this.formCreateProductOBJ.name = true;
+      }
+      if (this.formCreateProduct.controls["price"].errors.min) {
+        this.formCreateProductOBJ.price = true;
+      }
+      if (this.formCreateProduct.controls["stock"].errors.min) {
+        this.formCreateProductOBJ.stock = true;
+      }
+    }else{
       this.productService.saveProduct(this.formCreateProduct.value).subscribe((data: any) => {
         console.log(data);
       });
     }
   }
-
 }
